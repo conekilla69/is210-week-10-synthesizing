72728329b3bf1002afc4a1c6165547563eb99721
conekilla69 <wrcfan@verizon.net>
2015-11-01 20:09:28 +0000

    kept getting error, about a local variable, probably wrote the function wrong.

---
+++ task_01.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+"""Dictinoary entries"""
+
+
+
+CUSTOMERS = {
+    123: {'name': 'TJ', 'email':' docTJ@email.com'},
+    345: {'name': 'Nikki', 'emal': 'painnurse@email.com'},
+    234: {'name': 'Jenna', 'email': 'JLo@email.com'},
+    456: {'name': 'Brea', 'emal': 'Brea@email.com'}
+}
+
+ORDERS = {
+    1: {'customer_ID': 123, 'total': 5},
+    2: {'customer_ID': 345, 'total': 8},
+    3: {'customer_ID': 234, 'total': 12},
+    4: {'customer_ID': 456, 'total': 14}
+}
+
+def sum_orders(customers, orders):
+    """"Customer ID's as dictionary entries
+    customers = CUSTOMERS (dict): customers keyed with ID
+    orders = ORDERS (dict): dictionary of orders keyed by order ID
+    """
+
+    for value in sum_orders.iteritems():
+        return key, value
+        print key, value
+        sum_orders = zip(CUSTOMERS, ORDERS)
+        
+        
+    
+
+        
+    
